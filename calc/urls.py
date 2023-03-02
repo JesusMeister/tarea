3dd416c9b49b58e5a0abf6542b75b55baa5bc1b9
JesusMeister <88863350+JesusMeister@users.noreply.github.com>
2023-03-02 10:03:47 -0600

    Initial commit

---
+++ calc/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('proceso',views.proceso,name='proceso'),
+    path('suma',views.suma,name='suma'),
+    path('multiplicacion',views.multiplicacion, name='multiplicacion'),
+    path('division',views.division, name='division'),
+    path('resta',views.resta, name='resta'),
+]
